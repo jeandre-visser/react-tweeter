ffb33ff43042faf5c319504dd120a92479942b9e
Jeandre <jeandrev1414@gmail.com>
2022-08-05 16:57:54 -0600

    add prevent default method to input and form tags when clicked

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -5,10 +5,28 @@ function TweetForm(){
   return (
 
     <section className="newtweet">
-    <form method="post" action="/tweets" className="newtweet__form">
+    <form 
+      method="post" 
+      action="/tweets" 
+      className="newtweet__form"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      >
+
       <textarea className="form__textarea" name="text" placeholder={tweetPlaceholder}></textarea>
-      <input type="submit" value="Tweet" className="form__input" />
+
+      <input 
+        type="submit" 
+        value="Tweet" 
+        className="form__input" 
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+      />
+
       <span className="form__counter">140</span>
+      
     </form>
   </section>
   );
